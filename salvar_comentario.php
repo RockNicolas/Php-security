498167820817e679093e024a0c53a9dfdd55a400
NICOLAS ROCK PESSOA <60918958342@icc.local>
2025-03-10 16:47:34 -0300

    Finalizando curso de Buscando brechas em erros digitais dentro do php e servidores locais.

--- salvar_comentario.php
+++ salvar_comentario.php
@@ -1,13 +1,30 @@
 <?php
-$conn = mysql_connect("localhost", "root", "");
-$db = mysql_select_db("seguranca");
 
+require_once __DIR__ . '/vendor/autoload.php';
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+$host = $_ENV['DB_HOST'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
 
 $id_news = $_GET["id_news"];
 $nome = addslashes($_POST["txtNome"]);
 $comentario = addslashes($_POST["txtComentario"]);
+$sql = "INSERT INTO comentarios (nome, comentario, id_news) VALUES ('$nome', '$comentario', '$id_news')";
+$query = mysqli_query($conn, $sql);
 
-$sql = "INSERT INTO comentarios (nome, comentario, id_news) VALUES ('".$nome."', '".$comentario."', '".$id_news."')";
+if ($query) {
+    echo "Comment successfully added!";
+} else {
+    echo "Error: " . mysqli_error($conn);
+}
 
-$query = mysql_query($sql);
+mysqli_close($conn);
 ?>
